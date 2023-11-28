791c97a9c4b2e0f24bce5b71e77c22e9eccd8b66
BÁRBARA SCHVUCHOV KERN <barbara_kern@estudante.sesisenai.org.br>
2023-11-27 22:06:29 -0300

    commit inicial

---
+++ src/pages/Destiny/index.jsx
@@ -0,0 +1,15 @@
+
+
+import { Footer } from '../../componentes/Footer'
+
+export function Destiny(){
+    
+    return(
+        <>
+            <h1>Página Destino</h1>
+            {/* Footer */}
+            <Footer/>
+        </>
+    )
+}
+
